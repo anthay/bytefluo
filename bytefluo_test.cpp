8568dfbaab8920622638a05c66e2013b6bac4c2e
anthay <anthony.hay.1@gmail.com>
2017-03-23 20:19:02 +0000

    update copyright date and add LICENSE file

--- bytefluo_test.cpp
+++ bytefluo_test.cpp
@@ -2,7 +2,7 @@
 
         MIT License
 
-        Copyright (c) 2008,2009,2010,2016 Anthony C. Hay
+        Copyright (c) 2008,2009,2010,2016,2017 Anthony C. Hay
 
         Permission is hereby granted, free of charge, to any person obtaining a copy
         of this software and associated documentation files (the "Software"), to deal
